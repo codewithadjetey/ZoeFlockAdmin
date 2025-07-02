f4d76bd75dbf5878c421c06555937c7a38804b15
douglasderrick <douglasder12@gmail.com>
2025-07-02 18:20:37 +0000

    Add layout and login components for authentication pages

---
+++ frontend/src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div
+    className='flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200'
+    >layout</div>
+  )
+}
+
+export default layout;
