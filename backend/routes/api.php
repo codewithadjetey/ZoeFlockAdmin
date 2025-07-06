bfadd0c4d73c0084217fb801da630b9d6e732396
douglasderrick <douglasder12@gmail.com>
2025-07-06 18:51:22 +0000

    chore: clean up tracked files in storage directory, only keep .gitignore files

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\MemberController;
-use App\Http\Controllers\Api\DocumentationController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\MemberController;
+use App\Http\Controllers\Api\V1\DocumentationController;
 
 // Get the API version from config
 $apiVersion = config('app.version', 'v1');
