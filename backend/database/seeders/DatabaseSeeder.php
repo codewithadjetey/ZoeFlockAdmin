b45e6efc6a5a02ba429b473b7bf186c191c222c0
douglasderrick <douglasder12@gmail.com>
2025-08-12 00:30:17 +0000

    feat: Add MemberTableSeeder to populate members in the database
    - Introduce MemberTableSeeder to create and seed member records with associated user data
    - Update DatabaseSeeder to include MemberTableSeeder for proper seeding order

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             AdminSeeder::class,
+            MemberTableSeeder::class, // Add members after users exist
             GroupSeeder::class, // Add groups after users and members exist
         ]);
 
