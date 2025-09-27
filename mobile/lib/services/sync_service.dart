717874683321e1d86683e9e9261e80ed051cad1e
douglasderrick <douglasder12@gmail.com>
2025-09-27 16:04:43 +0000

    feat(orm): migrate services to use ORM-based DatabaseService
    - Update ScannerService to use ORM DatabaseService
    - Update SyncService to use ORM DatabaseService
    - Remove direct database access from service layer
    - Ensure all data operations go through ORM layer
    - Maintain service interface compatibility

--- mobile/lib/services/sync_service.dart
+++ mobile/lib/services/sync_service.dart
@@ -5,7 +5,7 @@ import '../models/member.dart';
 import '../models/event.dart';
 import '../utils/constants.dart';
 import 'api_service.dart';
-import 'database_service.dart';
+import 'database_service_orm.dart';
 import 'offline_attendance_service.dart';
 
 class SyncProgress {
